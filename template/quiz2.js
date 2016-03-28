1af51dfa42f08b4b4049bc7609b2d0bf5893c1d9
njs55 <njs55@pitt.edu>
2016-03-28 17:43:03 -0400

    Initial Commit
    some stuff is done

--- template/quiz2.js
+++ template/quiz2.js
@@ -1,39 +1,69 @@
-(function($){
+(function(){
 	// This is where you will write your function for the programming challenge
 	// Do not commit console.log statements
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	var mouseover = $('.mouse-over');
+	var click     = $('.click');
+	var sub       = $('.submit');
+   var time      = $('.timeout');
 
-	$mouseover.on('mouseover', function() {
+	mouseover.mouseover( function() {
 		$this = $(this);
 		$(this).html('Scrooge McDuck!');
-		$(this).height($(this).height() + 50);
+		//$(this).height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+	click.click(function() {
+		$(this).text('Peace Out!')
+		$(this).fadeOut(1500);
 		return false;
 	});
 
-	$submit.on('submit', function(e) {
+	sub.submit( function(e) {
+      console.log("submit");
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+			$(this).find('input').each(function() {
+				$(this).fadeOut("slow");
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).append("<h2>Congratulations! You've entered some text!</h2>");
 		}
 	});
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
+	time.ready( function() {
+		setTimeout(function(){
+         console.log("timeout");
+			$(this).fadeIn("slow");
 		}, 1000);
+      
 	});
 
-})(jQuery);
+})();
+
+/**
+ * calls $.get()
+ * no params
+ * no return
+ */
+function get(){
+   var topic = jQuery('#select-id');
+   $.get({
+      url: "http://www.mattbowytz.com/simple_api.json",
+      data: {
+         "data": "quizData"
+      },
+      success: function(data) {
+         console.log("data: " + data);
+         items = data.data;
+         console.log("items: " + items);
+         if (items.constructor === Object){
+            //merge both programming and interest Objects
+            items = $.merge(data.data.interests, data.data.programming);
+            //console.log("items2: " + items);
+         }
+            
+      }
+   });
+}
