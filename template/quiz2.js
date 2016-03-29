8de9267b930c2ccd8386befa5a7d7e92ff8eae01
njs55 <njs55@pitt.edu>
2016-03-28 23:13:40 -0400

    Added Comments and edited spacing
    so nice and pretty..sort of

--- template/quiz2.js
+++ template/quiz2.js
@@ -5,15 +5,16 @@
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
    items = null;
-	var mouseover = $('.mouse-over');
-	var click     = $('.click');
-	var sub       = $('.submit');
+   var mouseover = $('.mouse-over');
+   var click     = $('.click');
+   var sub       = $('.submit');
    var time      = $('.timeout');
    var getTitle  = jQuery('#get-id');
    var main      = jQuery('#main');
    var sec       = jQuery('#secondary');
    
-   
+   //check if a cookie has been set
+   //show/hide different divs based on if cookie is set
    if(document.cookie.length > 0){
       get();
       main.hide();
@@ -24,20 +25,22 @@
       sec.hide();
    }
    
-   //console.log(document.cookie);
-
+   //mouseover
 	mouseover.mouseover( function() {
 		$this = $(this);
 		$(this).html('Scrooge McDuck!');
 		//$(this).height($(this).height() + 50);
 	});
 
+   //click on text and change to "peace out" then fades out
 	click.click(function() {
 		$(this).text('Peace Out!')
 		$(this).fadeOut(1500);
 		return false;
 	});
 
+   //if you enter something in box it desplays congrats
+   //then we fade out the submit button and text field
 	sub.submit( function(e) {
       console.log("submit");
 		e.preventDefault();
@@ -49,6 +52,7 @@
 		}
 	});
 
+   //the word "timeout" is delayed when printed to console but not screen
 	time.ready( function() {
 		setTimeout(function(){
          //console.log("timeout");
@@ -56,23 +60,46 @@
 		}, 1000);
       
 	});
-
    
+   //when the Get Title button is clicked we hide the main div and show secondary div
+   //also call get() which makes an ajax request
    getTitle.on('click', function() {
       get();      
-      //hide main div
       main.hide();
-      //show secondary div
       sec.show();
    });
    
    
 })();
 
+/**makes the ajax call
+*sets the array items
+*calls sectiontwo() on success
+*/
+function get() {
+   $.get({
+      url: "http://www.mattbowytz.com/simple_api.json",
+      data: {
+         "data": "quizData"
+      },
+      success: function(data) {
+         items = data.data;
+         sectionTwo();
+      }
+   });
+}
+
+/**The start of section 2
+*it creates the Change it button
+*/
 function sectionTwo(){ 
    makeNewButton("change-id","Change It!",changeIt);
 }
 
+/**picks a random string from the items array
+*calls updateDisplay()
+*checks if Keep It button exists, if not it creates it
+*/
 function changeIt(){
    var string = randomString(items);
    updateDisplay(string);
@@ -81,10 +108,17 @@ function changeIt(){
    }
 }
 
+/**sets cookie by saving the word being displayed
+*/
 function keepIt(){
    document.cookie = document.getElementById("display").textContent;
 }
 
+/**makes a new button
+*param1 = desired ID
+*param2 = desired Name/value
+*param3 = desired function you want it to execute
+*/
 function makeNewButton(id,name,prop){
    var elem = document.createElement('input');
    elem.setAttribute('type', 'button');
@@ -102,24 +136,16 @@ function makeNewButton(id,name,prop){
    sec.appendChild(elem);
 }
 
+/**gets random string from array
+*/
 function randomString(){
    return items[Math.floor(Math.random() * items.length)];
 }
 
+/**updates display
+*param: string to be displayed
+*/
 function updateDisplay(string){
    var display = document.getElementById("display");
    display.innerHTML = "<h1>" + string + "</h1>";
-}
-
-function get() {
-   $.get({
-      url: "http://www.mattbowytz.com/simple_api.json",
-      data: {
-         "data": "quizData"
-      },
-      success: function(data) {
-         items = data.data;
-         sectionTwo();
-      }
-   });
 }
